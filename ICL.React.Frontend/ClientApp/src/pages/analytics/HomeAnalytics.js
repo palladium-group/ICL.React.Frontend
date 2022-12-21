36dffae4a47e99978b6215379411710718d73186
gitonga.kithinji <kithinjifelix@gmail.com>
2022-12-21 11:35:52 +0300

    update portal with dashboard generated why data warehouse team

---
+++ ICL.React.Frontend/ClientApp/src/pages/analytics/HomeAnalytics.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Analytics = () => {
+  return (
+    <iframe title="LIT ICL Dashboards - Overall Dashboard" width="100%" height="100%"
+            src="https://app.powerbi.com/view?r=eyJrIjoiOWU3M2I1ZDMtMzBlYS00NDBhLWJmZjYtZTI1Njk5YjFkZmRkIiwidCI6ImU3OTQyOTc0LTk3MzgtNGE0YS1iNjQ2LTJhYjkwZjc5ZGIwZiIsImMiOjF9"
+            frameBorder="0" allowFullScreen="true"></iframe>
+  );
+};
+export default Analytics;
