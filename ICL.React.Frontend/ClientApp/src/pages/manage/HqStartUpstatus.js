3c9d8a0913e3700f0786f572f84020ef10b3b01a
gitonga.kithinji <kithinjifelix@gmail.com>
2023-01-12 17:10:48 +0300

    add links to HQ Start Up

--- ICL.React.Frontend/ClientApp/src/pages/manage/HqStartUpstatus.js
+++ ICL.React.Frontend/ClientApp/src/pages/manage/HqStartUpstatus.js
@@ -2,14 +2,15 @@ import React from "react";
 import {Grid} from "@mui/material";
 
 
-const Manage = () => {
+const HqStartUpStatus = () => {
   return (
     <Grid container justifyContent="center" spacing={1} alignItems="stretch" sx={{ minHeight: "800px" }}>
       <Grid item md={12} zeroMinWidth>
-        <iframe title="HQ Start up - HQ startup Status" width="1024" height="1060"
+        <iframe title="HQ Start up - HQ startup Status" width="100%" height="100%"
                 src="https://app.powerbi.com/view?r=eyJrIjoiNWJjNjk2YWYtOGEwYS00YTU4LThlODUtNTRjMmJhNDhiNzhkIiwidCI6ImU3OTQyOTc0LTk3MzgtNGE0YS1iNjQ2LTJhYjkwZjc5ZGIwZiIsImMiOjF9"
                 frameBorder="0" allowFullScreen="true"></iframe>
       </Grid>
     </Grid>
   );
 };
+export default HqStartUpStatus;
