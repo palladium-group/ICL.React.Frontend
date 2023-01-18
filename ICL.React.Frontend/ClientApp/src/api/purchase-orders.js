d12e94b3361a940a261dcbf995ab9b40feeb12d8
Ken Kirui <31723768+kenkirui@users.noreply.github.com>
2023-01-18 12:53:31 +0300

    add getPurchaseOrderWithParam

--- ICL.React.Frontend/ClientApp/src/api/purchase-orders.js
+++ ICL.React.Frontend/ClientApp/src/api/purchase-orders.js
@@ -1,10 +1,17 @@
 import { apiRoutes } from "../apiRoutes";
 import axios from "axios";
 
-export const getPurchaseOrders = async () => {
+export const
+    s = async () => {
     return await axios.get(`${apiRoutes.purchaseOrder}`);
 };
 
 export const getCustomerOrders = async () => {
     return await axios.get(`${apiRoutes.customerOrder}`);
 };
+
+export const getPurchaseOrderWithParam = async ({ queryKey }) => {
+    const [, path] = queryKey;
+    return await axios.get(`${apiRoutes.customerOrder}/${path}`);
+};
+
