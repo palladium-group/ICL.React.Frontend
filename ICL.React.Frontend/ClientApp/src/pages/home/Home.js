7016e1d7af5b83ec9de1e94d6e38d8c35365ff4e
gitonga.kithinji <kithinjifelix@gmail.com>
2023-01-18 11:41:43 +0300

    update font type

--- ICL.React.Frontend/ClientApp/src/pages/home/Home.js
+++ ICL.React.Frontend/ClientApp/src/pages/home/Home.js
@@ -295,7 +295,7 @@ const SuccessContent=()=> {
                     <Typography component="div" variant="h5">
                       DHSC Framework
                     </Typography>
-                    <Typography variant="body2" color="text.secondary" component="div">
+                    <Typography variant="body2" component="div">
                       Our platform is one of a select few service providers supporting the UK Government’s short and long-term distribution of medical devices, consumables, reagents, and equipment across the country. The platform manages the storage of goods, provides a secure and user-friendly ordering system, and a means to collect payments in a UK primary care setting. Additionally, with ISO9000 certification for warehousing and 1so9001 for provision of customer-focused activities, the team can distribute medical devices and more to NHS centres, hospitals, and GP surgeries.
                       We provide an adaptive and scalable approach to resource management and a solution that can flex in real time in response to the volume and pace of demand for goods.
 
@@ -316,7 +316,7 @@ const SuccessContent=()=> {
                     <Typography component="div" variant="h5">
                       The Global Fund [Global Fund Supply Chain Health Services Spot Check Project (GFSCHSSC)]
                     </Typography>
-                    <Typography variant="body2" color="text.secondary" component="div">
+                    <Typography variant="body2" component="div">
                       Across 22 countries and with data from 800 health facilities, we support The Global Fund to embed a culture of performance management in each country’s health supply chains. We routinely track indicators for on-shelf availability, on-time-in-full delivery, commodities stocked according to plan, and timeliness of facility’s reporting to the LMIS. Our analyses support health programs and the ability of supply chain actors to monitor needs, identify risks, and track trends in individual country logistics operations to help inform systems strengthening priorities.
                       By supporting national program managers in monitoring the availability of essential health commodities and services at different points in the system, we have created an in-depth understanding of whether and how health facilities have adapted to the challenges of COVID-19.
                     </Typography>
