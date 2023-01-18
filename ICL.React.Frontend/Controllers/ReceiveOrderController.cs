373509f0334826bcb8a5d4470694435da2c541b7
gitonga.kithinji <kithinjifelix@gmail.com>
2023-01-18 10:56:52 +0300

    read xml using ExpandoObject

--- ICL.React.Frontend/Controllers/ReceiveOrderController.cs
+++ ICL.React.Frontend/Controllers/ReceiveOrderController.cs
@@ -10,6 +10,9 @@ using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
 using Microsoft.Extensions.Configuration;
+using System.Dynamic;
+using System.Xml.Linq;
+using static Azure.Core.HttpHeader;
 
 namespace ICL.React.Frontend.Controllers
 {
@@ -44,55 +47,125 @@ namespace ICL.React.Frontend.Controllers
                         asn.AppendLine(reader.ReadLine());
                 }
 
-                XmlDocument xmlDoc = new XmlDocument();
-                xmlDoc.LoadXml(asn.ToString());
+                XDocument doc = XDocument.Parse(asn.ToString()); //or XDocument.Load(path)
+                string jsonText = JsonConvert.SerializeXNode(doc);
+                dynamic dyn = JsonConvert.DeserializeObject<ExpandoObject>(jsonText);
 
-                XmlNode xmlNode = xmlDoc.SelectSingleNode("//Booking");
                 string asndata;
+                var booking = dyn.Message.Bookings.Booking;
 
-                XmlSerializer serial = new XmlSerializer(typeof(Booking));
-
-                using (XmlNodeReader reader = new XmlNodeReader(xmlNode))
+                //Booking booking = (Booking)serial.Deserialize(reader);
+                PurchaseOrder po = new PurchaseOrder();
+                List<Product> products = new List<Product>();
+                po.Id = Guid.NewGuid();
+                po.CreateDate = DateTime.Now;
+                po.uuid = Guid.NewGuid();
+                po.BookingNo = booking.BasicDetails.BookingNo;
+                po.BookingDate = DateTime.ParseExact(booking.BasicDetails.BookingDate.ToString(), "yyyyMMdd", CultureInfo.InvariantCulture);
+                po.AsnFile = asn.ToString();
+                if (booking.GetType().GetProperty("Services") != null)
                 {
-                    Booking booking = (Booking)serial.Deserialize(reader);
-                    PurchaseOrder po = new PurchaseOrder();
-                    List<Product> products = new List<Product>();
-                    po.Id = Guid.NewGuid();
-                    po.CreateDate = DateTime.Now;
-                    po.uuid = Guid.NewGuid();
-                    po.BookingNo = booking.BasicDetails.BookingNo;
-                    po.BookingDate = DateTime.ParseExact(booking.BasicDetails.BookingDate.ToString(), "yyyyMMdd", CultureInfo.InvariantCulture);
-                    po.AsnFile = asn.ToString();
                     po.ProcessType = booking.Services[0].ProcessType;
-                    po.PlaceOfReceipt = booking.BasicDetails.Movement.PlaceOfReceipt.Code;
-                    po.PlaceOfDelivery = booking.BasicDetails.Movement.PlaceOfDelivery.Code;
-                    po.DeliveryStatus = 0;
-                    po.SubmitStatus = "Available";
+                }
+                po.PlaceOfReceipt = booking.BasicDetails.Movement.PlaceOfReceipt.Code;
+                po.PlaceOfDelivery = booking.BasicDetails.Movement.PlaceOfDelivery.Code;
+                po.DeliveryStatus = 0;
+                po.SubmitStatus = "Available";
 
-                    foreach (var prod in booking.Products)
+                foreach (var prod in booking.Products)
+                {
+                    Product product = new Product();
+                    product.Id = Guid.NewGuid();
+                    product.CreateDate = DateTime.Now;
+                    product.uuid = Guid.NewGuid();
+                    product.PoUuid = po.uuid;
+                    product.LineItemId = "";
+                    if (prod.GetType().GetProperty("ProductCode") != null)
                     {
-                        Product product = new Product();
-                        product.Id = Guid.NewGuid();
-                        product.CreateDate = DateTime.Now;
-                        product.uuid = Guid.NewGuid();
-                        product.PoUuid = po.uuid;
-                        product.LineItemId = "";
                         product.ProductCode = prod.ProductCode.ToString();
+                    }
+                    if (prod.GetType().GetProperty("Quantity") != null && prod.GetType().GetProperty("Quantity") != null)
+                    {
                         product.Quantity = prod.Quantity.Value + " " + prod.Quantity.Uom;
+                    }
+                    if (prod.GetType().GetProperty("UnitDimension") != null)
+                    {
                         product.UnitDimension = prod.UnitDimension.Length + "*" + prod.UnitDimension.Width + "*" +
-                            prod.UnitDimension.Height + " " + prod.UnitDimension.Uom;
+                        prod.UnitDimension.Height + " " + prod.UnitDimension.Uom;
+                    }
+                    if (prod.GetType().GetProperty("UnitVolume") != null)
+                    {
                         product.UnitVolume = prod.UnitVolume.ToString();
+                    }
+                    if (prod.GetType().GetProperty("UnitWeight") != null)
+                    {
                         product.UnitWeight = prod.UnitWeight.GrossWeight + " " + prod.UnitWeight.Uom;
+                    }
+                    if (prod.GetType().GetProperty("UnitRate") != null)
+                    {
                         product.UnitRate = prod.UnitRate.ToString();
+                    }
+                    if (prod.GetType().GetProperty("OrderDetails") != null)
+                    {
                         product.OrderDetails = prod.OrderDetails.SKULineNo;
                         product.SKULineNo = prod.OrderDetails.SKULineNo;
-                        products.Add(product);
                     }
-
-                    po.products = products;
-
-                    asndata = JsonConvert.SerializeObject(po);
+                    products.Add(product);
                 }
+
+                po.products = products;
+
+                asndata = JsonConvert.SerializeObject(po);
+
+                //XmlDocument xmlDoc = new XmlDocument();
+                //xmlDoc.LoadXml(asn.ToString());
+
+                //XmlNode xmlNode = xmlDoc.SelectSingleNode("//Booking");
+                //string asndata;
+
+                //XmlSerializer serial = new XmlSerializer(typeof(Booking));
+
+                //using (XmlNodeReader reader = new XmlNodeReader(xmlNode))
+                //{
+                //    Booking booking = (Booking)serial.Deserialize(reader);
+                //    PurchaseOrder po = new PurchaseOrder();
+                //    List<Product> products = new List<Product>();
+                //    po.Id = Guid.NewGuid();
+                //    po.CreateDate = DateTime.Now;
+                //    po.uuid = Guid.NewGuid();
+                //    po.BookingNo = booking.BasicDetails.BookingNo;
+                //    po.BookingDate = DateTime.ParseExact(booking.BasicDetails.BookingDate.ToString(), "yyyyMMdd", CultureInfo.InvariantCulture);
+                //    po.AsnFile = asn.ToString();
+                //    po.ProcessType = booking.Services[0].ProcessType;
+                //    po.PlaceOfReceipt = booking.BasicDetails.Movement.PlaceOfReceipt.Code;
+                //    po.PlaceOfDelivery = booking.BasicDetails.Movement.PlaceOfDelivery.Code;
+                //    po.DeliveryStatus = 0;
+                //    po.SubmitStatus = "Available";
+
+                //    foreach (var prod in booking.Products)
+                //    {
+                //        Product product = new Product();
+                //        product.Id = Guid.NewGuid();
+                //        product.CreateDate = DateTime.Now;
+                //        product.uuid = Guid.NewGuid();
+                //        product.PoUuid = po.uuid;
+                //        product.LineItemId = "";
+                //        product.ProductCode = prod.ProductCode.ToString();
+                //        product.Quantity = prod.Quantity.Value + " " + prod.Quantity.Uom;
+                //        product.UnitDimension = prod.UnitDimension.Length + "*" + prod.UnitDimension.Width + "*" +
+                //            prod.UnitDimension.Height + " " + prod.UnitDimension.Uom;
+                //        product.UnitVolume = prod.UnitVolume.ToString();
+                //        product.UnitWeight = prod.UnitWeight.GrossWeight + " " + prod.UnitWeight.Uom;
+                //        product.UnitRate = prod.UnitRate.ToString();
+                //        product.OrderDetails = prod.OrderDetails.SKULineNo;
+                //        product.SKULineNo = prod.OrderDetails.SKULineNo;
+                //        products.Add(product);
+                //    }
+
+                //    po.products = products;
+
+                //    asndata = JsonConvert.SerializeObject(po);
+                //}
 
                 var bookingRequestContent = new StringContent(asndata, Encoding.UTF8, "application/json");
                 _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", null);
