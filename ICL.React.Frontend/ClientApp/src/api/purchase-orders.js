48e18f4517a4ee9d85ebb250c6f07aaa02c963a8
Ken Kirui <31723768+kenkirui@users.noreply.github.com>
2023-01-18 12:03:33 +0300

    separate inbound and outbound end-points

--- ICL.React.Frontend/ClientApp/src/api/purchase-orders.js
+++ ICL.React.Frontend/ClientApp/src/api/purchase-orders.js
@@ -4,3 +4,8 @@ import axios from "axios";
 export const getPurchaseOrders = async () => {
     return await axios.get(`${apiRoutes.purchaseOrder}`);
 };
+
+export const getPurchaseOrderWithParam = async ({ queryKey }) => {
+    const [, path] = queryKey;
+    return await axios.get(`${apiRoutes.purchaseOrder}/${path}`);
+};
