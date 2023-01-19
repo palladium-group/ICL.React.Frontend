7fef777da1b3cb81559bd38ef16d6c7edac74285
gitonga.kithinji <kithinjifelix@gmail.com>
2023-01-19 10:24:50 +0300

    automated reports addition

---
+++ ICL.React.Frontend/ClientApp/src/pages/enable/AutomatedReports.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {Grid} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+import styled from "@emotion/styled";
+import { spacing } from "@mui/system";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import {
+  Button as MuiButton,
+} from "@mui/material";
+import ReplyIcon from '@mui/icons-material/Reply';
+
+const Button = styled(MuiButton)(spacing);
+const theme = createTheme({
+  palette: {
+    secondary: {
+      main: "#05C3DE",
+    },
+  },
+});
+
+const AutomatedReports = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <Grid container spacing={6}>
+        <Grid item>
+          <ThemeProvider theme={theme}>
+            <Button
+              mr={2}
+              variant="contained"
+              color="secondary"
+              onClick={() => navigate("/enable")}
+            >
+              <ReplyIcon />
+            </Button>
+          </ThemeProvider>
+        </Grid>
+      </Grid>
+      <br />
+      <Grid container justifyContent="center" spacing={1} alignItems="stretch" sx={{ minHeight: "800px" }}>
+        <Grid item md={12} zeroMinWidth>
+          <iframe title="ICL Dashboards - Automated Reports" width="100%" height="100%"
+                  src="https://app.powerbi.com/view?r=eyJrIjoiNmRkYjU5N2UtMDNiMS00YmVkLWI3YjktNjI2YWEwNDQ0MmM3IiwidCI6ImU3OTQyOTc0LTk3MzgtNGE0YS1iNjQ2LTJhYjkwZjc5ZGIwZiIsImMiOjF9&pageName=ReportSectionabb0fc3d2b07206c01d0"
+                  frameBorder="0" allowFullScreen="true"></iframe>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+export default AutomatedReports;
