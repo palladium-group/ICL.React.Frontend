286b43b41b5187a7ad642d4b8d3363952c020b3f
gitonga.kithinji <kithinjifelix@gmail.com>
2023-01-19 18:33:35 +0300

    publish the changes by analytics

---
+++ ICL.React.Frontend/ClientApp/src/pages/enable/Warehouse.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {Grid} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+import styled from "@emotion/styled";
+import { spacing } from "@mui/system";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import {
+  Button as MuiButton,
+} from "@mui/material";
+import ReplyIcon from '@mui/icons-material/Reply';
+
+const Button = styled(MuiButton)(spacing);
+const theme = createTheme({
+  palette: {
+    secondary: {
+      main: "#05C3DE",
+    },
+  },
+});
+
+const Warehouse = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <Grid container spacing={6}>
+        <Grid item>
+          <ThemeProvider theme={theme}>
+            <Button
+              mr={2}
+              variant="contained"
+              color="secondary"
+              onClick={() => navigate("/enable")}
+            >
+              <ReplyIcon />
+            </Button>
+          </ThemeProvider>
+        </Grid>
+      </Grid>
+      <br />
+      <Grid container justifyContent="center" spacing={1} alignItems="stretch" sx={{ minHeight: "800px" }}>
+        <Grid item md={12} zeroMinWidth>
+          <iframe title="LIT ICL Dashboards - Warehousing - Navigation " width="100%" height="100%"
+                  src="https://app.powerbi.com/view?r=eyJrIjoiMzBhM2RmMjQtMTFmNi00NzM1LThhYWItYjI3YmIwYmFiOTc3IiwidCI6ImU3OTQyOTc0LTk3MzgtNGE0YS1iNjQ2LTJhYjkwZjc5ZGIwZiIsImMiOjF9"
+                  frameBorder="0" allowFullScreen="true"></iframe>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+export default Warehouse;
