c017d1dc4cf63a7bc1e9d19afce9d79d5edf975d
gitonga.kithinji <kithinjifelix@gmail.com>
2023-02-20 12:42:08 +0300

    StaffingLevelsHQ and SmallBusiness Utilization new forms

--- ICL.React.Frontend/ClientApp/src/pages/manage/SmallBusiness.js
+++ ICL.React.Frontend/ClientApp/src/pages/manage/SmallBusiness.js
@@ -10,6 +10,11 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@mui/material/styles";
 import ReplyIcon from '@mui/icons-material/Reply';
 import {createTheme, ThemeProvider} from "@mui/material/styles";
+import AddIcon from "@mui/icons-material/Add";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import Dialog from "@mui/material/Dialog";
 
 const Button = styled(MuiButton)(spacing);
 const themeCustom = createTheme({
@@ -24,33 +29,73 @@ const SmallBusiness = () => {
   const navigate = useNavigate();
   const theme = useTheme();
   const isLgUp = useMediaQuery(theme.breakpoints.up("lg"));
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Grid container p={isLgUp ? 12 : 5}>
-      <Grid item md={12}>
-        <Grid container spacing={6}>
-          <Grid item>
-            <ThemeProvider theme={themeCustom}>
-              <Button
-                mr={2}
-                variant="contained"
-                color="secondary"
-                onClick={() => navigate("/manage")}
-              >
-                <ReplyIcon />
-              </Button>
-            </ThemeProvider>
+    <React.Fragment>
+      <Grid container p={isLgUp ? 12 : 5}>
+        <Grid item md={12}>
+          <Grid container spacing={6}>
+            <Grid item md={10}>
+              <ThemeProvider theme={themeCustom}>
+                <Button
+                  mr={2}
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => navigate("/manage")}
+                >
+                  <ReplyIcon />
+                </Button>
+              </ThemeProvider>
+            </Grid>
+            <Grid item md={2} sx={{ margin: 0 }}>
+              <Grid container direction="row-reverse">
+                <Grid item>
+                  <ThemeProvider theme={themeCustom}>
+                    <Button
+                      mr={2}
+                      variant="contained"
+                      color="primary"
+                      onClick={handleClickOpen}
+                    >
+                      <AddIcon />
+                    </Button>
+                  </ThemeProvider>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          <br />
+          <Grid container justifyContent="center" spacing={1} alignItems="stretch" sx={{ minHeight: "800px" }}>
+            <Grid item md={12} zeroMinWidth>
+              <iframe title="LIT ICL Dashboards - Small Business Utilization" width="100%" height="100%"
+                      src="https://app.powerbi.com/view?r=eyJrIjoiZjI3MGM4OGYtN2UzNC00OWFlLWEzMTAtM2FhMWU1MzYxZGRlIiwidCI6ImU3OTQyOTc0LTk3MzgtNGE0YS1iNjQ2LTJhYjkwZjc5ZGIwZiIsImMiOjF9&pageName=ReportSection"
+                      frameBorder="0" allowFullScreen="true"></iframe>
+            </Grid>
           </Grid>
         </Grid>
-        <br />
-        <Grid container justifyContent="center" spacing={1} alignItems="stretch" sx={{ minHeight: "800px" }}>
-          <Grid item md={12} zeroMinWidth>
-            <iframe title="LIT ICL Dashboards - Small Business Utilization" width="100%" height="100%"
-                    src="https://app.powerbi.com/view?r=eyJrIjoiZjI3MGM4OGYtN2UzNC00OWFlLWEzMTAtM2FhMWU1MzYxZGRlIiwidCI6ImU3OTQyOTc0LTk3MzgtNGE0YS1iNjQ2LTJhYjkwZjc5ZGIwZiIsImMiOjF9&pageName=ReportSection"
-                    frameBorder="0" allowFullScreen="true"></iframe>
-          </Grid>
-        </Grid>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>GHSC ICL Small Business Utilization</DialogTitle>
+          <DialogContent>
+            <iframe width="640px" height="480px" title="GHSC ICL Small Business Utilization" src="https://forms.office.com/r/A0vkHmbMee?embed=true" frameBorder="0"
+                    marginWidth="0" marginHeight="0" style={{ border: "none", maxWidth: "100%", maxHeight: "100vh" }}
+                    allowFullScreen webkitallowfullscreen mozallowfullscreen msallowfullscreen></iframe>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Close</Button>
+          </DialogActions>
+        </Dialog>
       </Grid>
-    </Grid>
+    </React.Fragment>
   );
 };
 export default SmallBusiness;
