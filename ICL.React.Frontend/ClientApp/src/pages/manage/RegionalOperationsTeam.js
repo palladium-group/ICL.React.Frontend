5685c094b7a783b59f81582b5bd01affc8bd0e3c
gitonga.kithinji <kithinjifelix@gmail.com>
2023-04-17 12:54:10 +0300

    GHSC ICL Staffing Levels Cost Shared and Regional Operations

--- ICL.React.Frontend/ClientApp/src/pages/manage/RegionalOperationsTeam.js
+++ ICL.React.Frontend/ClientApp/src/pages/manage/RegionalOperationsTeam.js
@@ -10,6 +10,11 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@mui/material/styles";
 import ReplyIcon from '@mui/icons-material/Reply';
 import {createTheme, ThemeProvider} from "@mui/material/styles";
+import AddIcon from "@mui/icons-material/Add";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import Dialog from "@mui/material/Dialog";
 
 const Button = styled(MuiButton)(spacing);
 const themeCustom = createTheme({
@@ -25,11 +30,22 @@ const RegionalOperationsTeam = () => {
     const navigate = useNavigate();
     const theme = useTheme();
     const isLgUp = useMediaQuery(theme.breakpoints.up("lg"));
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
     return (
       <Grid container p={isLgUp ? 12 : 5}>
         <Grid item md={12}>
           <Grid container spacing={6}>
-            <Grid item>
+            <Grid item md={10}>
               <ThemeProvider theme={themeCustom}>
                 <Button
                   mr={2}
@@ -40,6 +56,22 @@ const RegionalOperationsTeam = () => {
                   <ReplyIcon />
                 </Button>
               </ThemeProvider>
+            </Grid>
+            <Grid item md={2} sx={{ margin: 0 }}>
+              <Grid container direction="row-reverse">
+                <Grid item>
+                  <ThemeProvider theme={themeCustom}>
+                    <Button
+                      mr={2}
+                      variant="contained"
+                      color="primary"
+                      onClick={handleClickOpen}
+                    >
+                      <AddIcon />
+                    </Button>
+                  </ThemeProvider>
+                </Grid>
+              </Grid>
             </Grid>
           </Grid>
           <br />
@@ -55,6 +87,17 @@ const RegionalOperationsTeam = () => {
             </Grid>
           </Grid>
         </Grid>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>GHSC ICL Staffing Levels Cost Shared and Regional Operations</DialogTitle>
+          <DialogContent>
+            <iframe width="640px" height="480px" title="GHSC ICL Staffing Levels Cost Shared and Regional Operations" src="https://forms.office.com/r/faLYHAmpMB?embed=true" frameBorder="0"
+                    marginWidth="0" marginHeight="0" style={{ border: "none", maxWidth: "100%", maxHeight: "100vh" }}
+                    allowFullScreen webkitallowfullscreen mozallowfullscreen msallowfullscreen></iframe>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Close</Button>
+          </DialogActions>
+        </Dialog>
       </Grid>
     );
 };
